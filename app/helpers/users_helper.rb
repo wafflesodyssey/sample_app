f3efc3ed29e359a241e782357aa9fd83da89fe6b
tony un <tony un>
2016-10-14 15:54:03 -0400

    Add resource for Users. Add show action in User controller. Add a show view for Users. Add a debug output in custom SCSS file. Also add gravatar_for helper method.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  # This defined action returns the Gravatar for the given user.
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexidigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
