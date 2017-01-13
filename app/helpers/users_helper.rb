85b3e64d9a6a5ed5d857c2ee5f9227915077e0ac
tony un <tony un>
2017-01-13 15:49:07 -0500

    Add comment for options hash in gravatar_for. Add ERB for time in show page.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
 
   # This defined action returns the Gravatar for the given user.
-  def gravatar_for(user, options = {size: 80})
+  def gravatar_for(user, options = {size: 80})  #arguments with no options is (user, size: 80)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
+    size = options[:size] #this gives an options hash in gravatar_for
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
