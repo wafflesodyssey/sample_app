e04f9da7aa7ef1e82fadb55df88bd47f86e54930
tony un <tony un>
2017-02-23 18:09:41 -0500

    Add forwarding and index for all users.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,8 +3,13 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:michael)
+    @user       = users(:michael)
     @other_user = users(:archer)
+  end
+
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
   end
 
   test "should redirect edit when not logged in" do
