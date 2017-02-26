4c0877df572a0b9c30c1ea07b21f5a30e5bf4852
tony un <tony un>
2017-02-26 13:24:29 -0500

    Add tests and method for account activation.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,8 +29,8 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'localhost:3000'
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  host = 'localhost:3000'  # Local, host of development environment.
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' } # 'https' for Cloud
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
