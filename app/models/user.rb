ffd1019638dd9d46c3c8ab3e21c32c29b75dc2de
tony un <tony un>
2017-02-28 22:09:15 -0500

    Add password reset.

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,6 +64,11 @@ class User < ApplicationRecord
     def forget
       update_attribute(:rememeber_digest, nil)
     end
+
+    # Returns true if a password reset has expired.
+    def password_reset_expired?
+      reset_sent_at < 2.hours.ago
+    end
   end
 end
 
