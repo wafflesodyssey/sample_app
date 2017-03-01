20adb3d40e796c4a85831d6478abb90e645c4643
tony un <tony un>
2017-03-01 17:25:16 -0500

    Add order for micropost.

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,8 +4,7 @@ class MicropostTest < ActiveSupport::TestCase
 
   def setup
     @user = users(:michael)
-
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
    test "should be valid" do
@@ -23,7 +22,11 @@ class MicropostTest < ActiveSupport::TestCase
    end
 
    test "content should be at most 140 characters" do
-     @micropost.content = "a" * 140
+     @micropost.content = "a" * 141
      assert_not @micropost.valid?
+   end
+
+   test "order should be most recent first" do
+     assert_equal microposts(:most_recent), Micropost.first
    end
 end
