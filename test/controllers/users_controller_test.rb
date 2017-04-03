44e36465f5c08460c7b1fbe9a5d4fd55a3d1b15c
tony un <tony un>
2017-04-03 19:49:43 -0400

    Add form for following/unfollowing.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:archer)
   end
 
+  test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
+  end
+
   test "should redirect index when not logged in" do
     get users_path
     assert_redirected_to login_url
