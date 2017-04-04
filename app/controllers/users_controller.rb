e4cf92883a637f83281149f0b379e5e8ec84e015
tony un <tony un>
2017-04-03 21:05:05 -0400

    Add controller for Relationships.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
@@ -17,7 +18,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)  
+    @user = User.new(user_params)
     if @user.save #if successful with valid signup data, will save
       @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
@@ -43,6 +44,20 @@ class UsersController < ApplicationController
       User.find(params[:id]).destroy
       flash[:success] = "User deleted"
       redirect_to users_url
+    end
+
+    def following
+      @title = "Following"
+      @user = User.find(params[:id])
+      @users = @user.following.paginate(page: params[:page])
+      render 'show_follow'
+    end
+
+    def followers
+      @title = "Followers"
+      @user = User.find(params[:id])
+      @users = @user.followers.paginate(page: params[:page])
+      render 'show_follow'
     end
 
     private
